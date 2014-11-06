8487489c40ae2d1f457c5daae78229fde92094f5
Steven <stvnksslr@gmail.com>
2014-11-06 18:47:48 -0500

    adding in kills changes to grab from zkillboard

--- app/kills/kills.js
+++ app/kills/kills.js
@@ -12,3 +12,20 @@
             return module;
         });
 })();
+
+        function LossesCtrl($scope, $http) {
+        	
+           this.someValue  = 'Some Value!';
+
+           // variable does an ajax request on the zkillboard page
+           var loadKills = $http.get('https://zkillboard.com/api/losses/no-attackers/allianceID/1354830081/');
+
+           // loads kills into parsable format outputs it to the logs
+           loadKills.then(function(response){
+               var kills = response.data;
+               console.log(kills);
+           });
+
+        }
+
+        module.controller('LossesCtrl', ['$scope', '$http', LossesCtrl]);
