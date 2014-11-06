f61172062b29b8f026781224249e09087e5ecccb
Steven <stvnksslr@gmail.com>
2014-11-06 18:29:24 -0500

    fixing missing comma in space-tools.js and adding console logging to kills.js

--- app/kills/kills.js
+++ app/kills/kills.js
@@ -3,12 +3,14 @@
     var moduleName = 'space-tools.kills',
         angularDependencies = [];
     define([
-        'require',
-        'angular'
-    ], function(require, angular) {
-        var module = angular.module(moduleName, angularDependencies);
-        return module;
-    });
+            'require',
+            'angular',
+             console.log('i was loaded!');
+        ],
+        function(require, angular) {
+            var module = angular.module(moduleName, angularDependencies);
+            return module;
+        });
 })();
 
 var moduleName = 'space-tools.kills',
--- app/space-tools.js
+++ app/space-tools.js
@@ -6,7 +6,7 @@
 
     define([
         'require',
-        'angular'
+        'angular',
         './kills/kills'
     ], function(require, angular) {
 
