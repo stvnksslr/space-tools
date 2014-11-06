f037cb247204432d20a5cf0e384efe6cccc721c8
Steven <stvnksslr@gmail.com>
2014-11-06 18:12:49 -0500

    adding code to kills.js

---
+++ app/kills/kills.js
@@ -0,0 +1,15 @@
+(function() {
+    'use strict';
+    var moduleName = 'space-tools.kills',
+        angularDependencies = [];
+    define([
+        'require',
+        'angular'
+    ], function(require, angular) {
+        var module = angular.module(moduleName, angularDependencies);
+        return module;
+    });
+})();
+
+
+var moduleName = 'space-tools.kills',
