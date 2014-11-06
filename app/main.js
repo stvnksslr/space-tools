3a071ecf365bc238d5664abc6991240203852e40
s_kessler <stvnksslr@gmail.com>
2014-11-06 16:46:40 -0500

    adding main.js

---
+++ app/main.js
@@ -0,0 +1,26 @@
+require.config({
+    paths: {
+        'angular': '../bower_components/angular/angular',
+        // 'lodash': '../bower_components/lodash/dist/lodash', // You should probably use lodash
+        // 'angular.ui': '../bower_components/angular-ui/build/angular-ui', // You might want to use angular-ui
+        // 'ui.router': '../bower_components/angular-ui-router/release/angular-ui-router', You should probably use ui.router
+        // 'angular-loading-bar': '../bower_components/angular-loading-bar/build/loading-bar', // This is :coal:
+        // 'jquery': '../bower_components/jquery/dist/jquery', // Lets hope you dont need this
+    },
+    // 'Shims' are required for 3rd party libs that don't use some sort of AMD/CommonJS/UMD module definition
+    shim: {
+        'angular': {
+            exports: 'angular',
+            // deps: ['jquery'] // We don't need the features of jquery for now, but uncomment this if we do
+        }
+        // ,
+        // 'angular-loading-bar': ['angular', 'ngAnimate'],
+        // 'angular.ui': ['angular'],
+        // 'ui.router': ['angular'],
+    }
+});
+
+
+require(['space-tools'], function() {
+    console.log('Require Configured, Space Tools loaded');
+});
