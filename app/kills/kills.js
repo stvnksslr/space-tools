0db5b7c51fef0815c7dce167a94ddeb4681da684
Steven <stvnksslr@gmail.com>
2014-11-06 19:25:23 -0500

    finished copying pasting surfaces code incorrecty for the 5th time

--- app/kills/kills.js
+++ app/kills/kills.js
@@ -1,36 +1,33 @@
 (function() {
     'use strict';
+
     var moduleName = 'space-tools.kills',
+
         angularDependencies = [];
+
     define([
-            'require',
-            'angular'
-        ],
-        function(require, angular) {
-            var module = angular.module(moduleName, angularDependencies);
-            console.log('i was loaded!');
+        'require',
+        'angular'
+    ], function(require, angular) {
 
+        var module = angular.module(moduleName, angularDependencies);
 
-            function LossesCtrl($scope, $http) {
+        function LossesCtrl($scope, $http) {
 
-                loadKills.then(function(response) {
-                    var kills = response.data;
-                    this.kills = kills;
-                    console.log(kills);
-                }.bind(this));
+            var loadKills = $http.get('https://zkillboard.com/api/losses/no-attackers/allianceID/1354830081/');
 
-                // variable does an ajax request on the zkillboard page
-                var loadKills = $http.get('https://zkillboard.com/api/losses/no-attackers/allianceID/1354830081/');
+            loadKills.then(function(response) {
+                var kills = response.data;
+                this.kills = kills;
+                console.log(kills);
+            }.bind(this));
 
-                // loads kills into parsable format outputs it to the logs
-                loadKills.then(function(response) {
-                    var kills = response.data;
-                    console.log(kills);
-                });
+        }
 
-            }
-            module.controller('LossesCtrl', ['$scope', '$http', LossesCtrl]);
+        console.log('i was loaded!');
 
-            return module;
-        });
+        module.controller('LossesCtrl', ['$scope', '$http', LossesCtrl]);
+
+        return module;
+    });
 })();
