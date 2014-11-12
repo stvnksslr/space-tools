53f45081a897d8da2795f978330ff18a2f7e4a10
Tom Neyland <tcneyland+github@gmail.com>
2014-11-12 18:15:29 +0000

    adds shipTypeNames

--- app/kills/kills.js
+++ app/kills/kills.js
@@ -14,12 +14,19 @@
 
         function LossesCtrl($scope, $http) {
 
-            var loadKills = $http.get('https://zkillboard.com/api/losses/no-attackers/allianceID/1354830081/');
+            var loadKills = $http.get('https://zkillboard.com/api/losses/no-attackers/allianceID/1354830081/'),
+                loadTypeNames = $http.get('app/assets/invTypeNames.json');
 
             loadKills.then(function(response) {
                 var kills = response.data;
                 this.kills = kills;
                 console.log(kills);
+            }.bind(this));
+
+            loadTypeNames.then(function(response) {
+                var typeNames = response.data;
+                this.typeNames = typeNames;
+                console.log(typeNames);
             }.bind(this));
 
         }
