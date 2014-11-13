d801d979f911e2f8b03709ffd73c6d80196aaa2f
Steven Kessler <florecentfish@gmail.com>
2014-11-13 17:07:47 -0500

    durrrrrrrrrrrrrr

--- app/main.js
+++ app/main.js
@@ -1,6 +1,6 @@
 require.config({
     paths: {
-        'angular': '../bower_components/angular/angular'
+        'angular': '../bower_components/angular/angular',
         // 'lodash': '../bower_components/lodash/dist/lodash', // You should probably use lodash
         // 'angular.ui': '../bower_components/angular-ui/build/angular-ui', // You might want to use angular-ui
          'ui.router': '../bower_components/angular-ui-router/release/angular-ui-router',
