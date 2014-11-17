8566181c17741633ea4c6b5c6a6b1e976d6e25b3
Steven <florecentfish@gmail.com>
2014-11-16 23:29:47 -0500

    adding directive back in

--- app/kills/hello.js
+++ app/kills/hello.js
@@ -12,7 +12,16 @@
     ], function(require, angular) {
 
         var module = angular.module(moduleName, angularDependencies);
-        console.log('another attempt at getting this working');
+        .directvive('helloWorld', function) {
+        	return {
+        		controller: function($scope){
+        			alert('controller');
+        		}
+
+        	}
+        }
+
+
 
         return module;
     });
