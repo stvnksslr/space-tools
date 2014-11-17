dbb639baee58b3532fd6dbe2ef0d87597a1f9d86
Steven <florecentfish@gmail.com>
2014-11-17 00:48:29 -0500

    adding in a pilot variable that will one day be used to query pilots rather than an alliance

--- app/kills/kills.js
+++ app/kills/kills.js
@@ -17,6 +17,7 @@
         function LossesCtrl($scope, $http, $q) {
 
             var loadKills = $http.get('https://zkillboard.com/api/losses/no-attackers/allianceID/1354830081/'),
+                loadPilot = $http.get('https://zkillboard.com/api/kills/characterID/1564471258/'),
                 loadTypeNames = $http.get('app/assets/invTypeNames.json');
 
             // Using $q.all we can wait until all of the data is loaded before
@@ -24,16 +25,20 @@
             // We wait untill all data is loaded so that the template doesn't render too early
             $q.all({
                 loadKills: loadKills,
+                loadPilot: loadPilot,
                 loadTypeNames: loadTypeNames
             }).then(function(results) {
 
                 var kills = results.loadKills.data,
+                    pilot = results.loadPilot.data,
                     typeNames = results.loadTypeNames.data;
 
                 console.log(kills);
+                console.log(pilot);
                 console.log(typeNames);
 
                 this.kills = kills;
+                this.pilot = pilot;
                 this.typeNames = typeNames;
 
             }.bind(this));
