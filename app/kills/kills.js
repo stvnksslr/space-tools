b6177f835e1e66c71187297e33991b6ab74f103d
Steven <florecentfish@gmail.com>
2014-11-17 00:51:03 -0500

    changing kills to alliance so that the division and scoping of searches makes more sense when i start passing variables to the url's as a search rather than have them hard coded certain individuals.

--- app/kills/kills.js
+++ app/kills/kills.js
@@ -16,7 +16,7 @@
 
         function LossesCtrl($scope, $http, $q) {
 
-            var loadKills = $http.get('https://zkillboard.com/api/losses/no-attackers/allianceID/1354830081/'),
+            var loadAlliance = $http.get('https://zkillboard.com/api/losses/no-attackers/allianceID/1354830081/'),
                 loadPilot = $http.get('https://zkillboard.com/api/kills/characterID/1564471258/'),
                 loadTypeNames = $http.get('app/assets/invTypeNames.json');
 
@@ -24,20 +24,20 @@
             // setting it on this controller.
             // We wait untill all data is loaded so that the template doesn't render too early
             $q.all({
-                loadKills: loadKills,
+                loadAlliance: loadAlliance,
                 loadPilot: loadPilot,
                 loadTypeNames: loadTypeNames
             }).then(function(results) {
 
-                var kills = results.loadKills.data,
+                var alliance = results.loadAlliance.data,
                     pilot = results.loadPilot.data,
                     typeNames = results.loadTypeNames.data;
 
-                console.log(kills);
+                console.log(alliance);
                 console.log(pilot);
                 console.log(typeNames);
 
-                this.kills = kills;
+                this.alliance = alliance;
                 this.pilot = pilot;
                 this.typeNames = typeNames;
 
