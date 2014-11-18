006c61fbdbead1a037047edd552af8c3709311a6
Steven Kessler <skessler@crownawards.com>
2014-11-18 13:29:20 -0500

    pilotStats.js

--- app/kills/pilotStats.js
+++ app/kills/pilotStats.js
@@ -8,7 +8,7 @@
     ], function(require, angular) {
         var module = angular.module(moduleName, angularDependencies);
 
-        module.directive('killmail', function() {
+        module.directive('pilotStats', function() {
             return {
                 templateUrl: require.toUrl('./_pilotStats.html'),
                 controller: function($scope) {
