089c100a915313598ca37eff04c8dfb52b93980d
stvnksslr <stvnksslr@gmail.com>
2014-12-08 10:15:26 -0500

    nested router for km-tools setup

---
+++ app/km-tools/kmTools-router.js
@@ -0,0 +1,22 @@
+/**
+ * Created by skessler on 12/1/14.
+ */
+
+(function() {
+    'use strict';
+
+    var moduleName = 'space-tools.kmtools-router',
+
+        angularDependencies = ['ui.router'];
+
+    define([
+        'require',
+        'angular',
+        'ui.router'
+    ], function(require, angular, uirouter) {
+
+        var module = angular.module(moduleName, angularDependencies);
+
+        return module;
+    });
+})();
