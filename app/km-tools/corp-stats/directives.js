7b2d4a591a8f136968ff2a0fcbd94251d5b26a83
stvnksslr <stvnksslr@gmail.com>
2015-01-13 16:55:18 -0500

    adding corp level queries

---
+++ app/km-tools/corp-stats/directives.js
@@ -0,0 +1,21 @@
+/**
+ * Created by skessler on 1/13/15.
+ */
+'use strict';
+/**
+ * Created by skessler on 12/11/14.
+ */
+import * as angular from 'angular';
+
+var corpStatDirectives = angular.module('space-tools.km-tools.corp-stats.directives', []);
+
+// pilotStatDirectives.directive('pilotStats', function() {
+//     return {
+//         controller: function($scope) {
+//             this.pilot;
+//         }
+//     };
+// });
+
+
+export default corpStatDirectives;
---
+++ app/km-tools/corp-stats/index.js
@@ -0,0 +1,59 @@
+/**
+ * Created by skessler on 1/13/15.
+ */
+import * as angular from 'angular';
+import * as uiRouter from 'angular-ui-router';
+import * as fs from 'fs';
+
+var pilotStats = angular.module('space-tools.km-tools.corp-stats', ['ui.router']);
+
+function PilotStatsCtrl($scope, $http, $q) {
+
+    var loadAlliance = $http.get('https://zkillboard.com/api/losses/no-attackers/allianceID/1354830081/'),
+        loadCorp = $http.get('https://zkillboard.com/api/losses/no-attackers/corporation/98342574/'),
+        loadPilot = $http.get('https://zkillboard.com/api/kills/characterID/1564471258/'),
+        loadTypeNames = $http.get('app/localAssets/invTypeNames.json');
+
+    // Using $q.all we can wait until all of the data is loaded before
+    // setting it on this controller.
+    // We wait untill all data is loaded so that the template doesn't render too early
+    $q.all({
+        loadAlliance: loadAlliance,
+        loadCorp: loadCorp,
+        loadPilot: loadPilot,
+        loadTypeNames: loadTypeNames
+    }).then(function(results) {
+
+        var alliance = results.loadAlliance.data,
+            corp = results.loadCorp.data,
+            pilot = results.loadPilot.data,
+            typeNames = results.loadTypeNames.data;
+
+        console.log(alliance);
+        console.log(corp);
+        console.log(pilot);
+        console.log(typeNames);
+
+        this.alliance = alliance;
+        this.corp = corp;
+        this.pilot = pilot;
+        this.typeNames = typeNames;
+
+    }.bind(this));
+
+}
+
+// Register out PilotStatsCtrl
+pilotStats.controller('CorpStatsCtrl', ['$scope', '$http', '$q', CorpStatsCtrl]);
+
+// Define the states we want to expose for this submodule
+pilotStats.config(['$stateProvider', function($stateProvider) {
+    $stateProvider.state('km-tools.corp-stats', {
+        url: '/corp-stats',
+        template: require('./_corpStats.html'),
+        controller: 'corpStatsCtrl',
+        controllerAs: 'CorpStats'
+    });
+}]);
+
+export default pilotStats;
