dee962c98e6a2fc99c074874109c301cf4ed08f8
Tom Neyland <tcneyland+github@gmail.com>
2015-01-13 04:32:33 +0000

    WIP: renames pilot-stats/pilotStats to ./directives, I think that is what the intention was

---
+++ app/km-tools/pilot-stats/directives.js
@@ -0,0 +1,18 @@
+'use strict';
+/**
+ * Created by skessler on 12/11/14.
+ */
+import * as angular from 'angular';
+
+var pilotStatDirectives = angular.module(moduleName, angularDependencies);
+
+// pilotStatDirectives.directive('pilotStats', function() {
+//     return {
+//         controller: function($scope) {
+//             this.pilot;
+//         }
+//     };
+// });
+
+
+export default pilotStatDirectives;
