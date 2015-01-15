0915a8a1ced8f3b5459fa23f56a42baccb0e8ca1
stvnksslr <stvnksslr@gmail.com>
2015-01-15 11:28:36 -0500

    added the alliance level querying to the app, wrote out the alliance stats based on the template set by corp and pilot stats

---
+++ app/km-tools/alliance-stats/directives.js
@@ -0,0 +1,9 @@
+/**
+ * Created by skessler on 1/15/15.
+ */
+'use strict';
+import * as angular from 'angular';
+
+var allianceStatDirectives = angular.module('space-tools.km-tools.alliance-stats.directives', []);
+
+export default allianceStatDirectives;
---
+++ app/km-tools/alliance-stats/index.js
@@ -0,0 +1,57 @@
+'use strict';
+import * as angular from 'angular';
+import * as uiRouter from 'angular-ui-router';
+import * as fs from 'fs';
+
+var allianceStats = angular.module('space-tools.km-tools.alliance-stats', ['ui.router']);
+
+function AllianceStatsCtrl($scope, $http, $q) {
+
+    var loadAlliance = $http.get('https://zkillboard.com/api/losses/no-attackers/allianceID/1354830081/'),
+        loadCorp = $http.get('https://zkillboard.com/api/losses/no-attackers/corporation/98342574/'),
+        loadPilot = $http.get('https://zkillboard.com/api/kills/characterID/1564471258/'),
+        loadTypeNames = $http.get('app/localAssets/invTypeNames.json');
+
+    // Using $q.all we can wait until all of the data is loaded before
+    // setting it on this controller.
+    // We wait untill all data is loaded so that the template doesn't render too early
+    $q.all({
+        loadAlliance: loadAlliance,
+        loadCorp: loadCorp,
+        loadPilot: loadPilot,
+        loadTypeNames: loadTypeNames
+    }).then(function(results) {
+
+        var alliance = results.loadAlliance.data,
+            corp = results.loadCorp.data,
+            pilot = results.loadPilot.data,
+            typeNames = results.loadTypeNames.data;
+
+        console.log(alliance);
+        console.log(corp);
+        console.log(pilot);
+        console.log(typeNames);
+
+        this.alliance = alliance;
+        this.corp = corp;
+        this.pilot = pilot;
+        this.typeNames = typeNames;
+
+    }.bind(this));
+
+}
+
+// Register out CorpStatsCtrl
+allianceStats.controller('AllianceStatsCtrl', ['$scope', '$http', '$q', AllianceStatsCtrl]);
+
+// Define the states we want to expose for this submodule
+allianceStats.config(['$stateProvider', function($stateProvider) {
+    $stateProvider.state('km-tools.alliance-stats', {
+        url: '/alliance-stats',
+        template: require('./_alliance-stats.html'),
+        controller: 'AllianceStatsCtrl',
+        controllerAs: 'AllianceStats'
+    });
+}]);
+
+export default allianceStats;
