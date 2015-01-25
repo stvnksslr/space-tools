2430e62491ca306bd4e7b0a37d1acf14c4f60b5a
stvnksslr <stvnksslr@gmail.com>
2015-01-25 14:14:17 -0500

    update for jokine to see what im working on, pretty sure i have a fever and im too tired to make a proper commit message. but im basically still trying to pass the character id down the chain so that the uri-sef branching i made off the table rows will end up displaying actual information about that specific persons death

--- app/km-tools/kill-details/index.js
+++ app/km-tools/kill-details/index.js
@@ -6,6 +6,34 @@ import * as uiRouter from 'angular-ui-router';
 var killDetails = angular.module('space-tools.km-tools.kill-details', ['ui.router']);
 
 
+function KillDetailsCtrl($scope, $http, $q) {
+
+    var loadPilot = $http.get('https://zkillboard.com/api/kills/characterID/1564471258/'),
+        loadTypeNames = $http.get('http://public-crest.eveonline.com/types/');
+
+    // Using $q.all we can wait until all of the data is loaded before
+    // setting it on this controller.
+    // We wait untill all data is loaded so that the template doesn't render too early
+    $q.all({
+        loadPilot: loadPilot,
+        loadTypeNames: loadTypeNames
+    }).then(function(results) {
+
+        var pilot = results.loadPilot.data,
+            typeNames = results.loadTypeNames.data;
+
+        console.log(pilot);
+        console.log(typeNames);
+
+        this.pilot = pilot;
+        this.typeNames = typeNames;
+
+    }.bind(this));
+
+}
+
+killDetails.controller('KillDetailsCtrl', ['$scope', '$http', '$q', KillDetailsCtrl]);
+
 killDetails.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
 
     $urlRouterProvider.otherwise('/home');
